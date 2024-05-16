434815750a4e1831a44b11d6dccf3e437d0a8f51
AzirEmperorShurima <tranvantri352@gmail.com>
2024-05-16 10:52:43 +0700

    commit

--- ProG_BE/app.js
+++ ProG_BE/app.js
@@ -1,5 +1,10 @@
 import express from 'express';
 const app = express();
-const PORT = process.env.PORT || 4000;
 
+app.set("env", "development");
+app.set("port", process.env.PORT || 4000)
+app.set('trust proxy', true)
+app.set('json spaces', 4);
+app.set('case sensitive routing', true)
+app.set('strict routing', true)
 export default app
