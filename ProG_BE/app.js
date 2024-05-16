77d468f8b1d4856644e19b7d8eb67893916baa2c
AzirEmperorShurima <tranvantri352@gmail.com>
2024-05-16 11:10:29 +0700

    22

--- ProG_BE/app.js
+++ ProG_BE/app.js
@@ -1,10 +1,20 @@
 import express from 'express';
 const app = express();
-
+const router = express.router();
 app.set("env", "development");
 app.set("port", process.env.PORT || 4000)
 app.set('trust proxy', true)
 app.set('json spaces', 4);
 app.set('case sensitive routing', true)
 app.set('strict routing', true)
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 export default app
